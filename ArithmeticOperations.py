1d76b911ce5250d5661e1602e6ae0b045965c29e
jtithy <tithyjebunnesa202@gmail.com>
2026-07-10 20:05:47 +0600

    Arithmetic Operations in Python

---
+++ ArithmeticOperations.py
@@ -0,0 +1,24 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Program to implement arithmetic operations.
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+#Sumation
+c = a+b
+print(f"The sum of {a} and {b} is {c}")
+#Subtraction
+c = a-b
+print(f"The subtraction of {a} and {b} is {c}")
+#Multiplication
+c = a*b
+print(f"The multiplication of {a} and {b} is {c}")
+#Division
+c =a/b
+print(f"The division of {a} and {b} is {c}")
+#Remainder
+c = a%b
+print(f"The remainder of {a} and {b} is {c}")
+#Exponentiation
+c = a**b
+print(f"The exponentiation of {a} and {b} is {c}")
