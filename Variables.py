c598fc62e7b12cdea9fb177b6b97b50b8a97543d
jtithy <tithyjebunnesa202@gmail.com>
2026-07-10 09:11:38 +0600

    Variable diclaration

---
+++ Variables.py
@@ -0,0 +1,10 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Variables.py file for storing different variables
+
+
+name = "Tithy"
+age= 23
+target = 3.5
+is_new = False
+print('My name\'s ' +name+ ' and I\'m ' +str(age)+ ' years old. I\'ll complete full basic python programming in ' +str(target)+ ' months.')
