bad696263fc9ec5ea65bffd38d4583d519262a00
jtithy <tithyjebunnesa202@gmail.com>
2026-07-10 19:23:36 +0600

    Solved a simple task in python

---
+++ Task1.py
@@ -0,0 +1,7 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Ask a user their weight(in pounds), convert it to kilograms and print on ther terminal.
+
+weight = input('Enter your weight in pounds: ')
+weight_kg = int(weight) * .453
+print('Your weight is in kilograms: ' +str(weight_kg))
