edb1fe89c46c4a2635e92979acd459d0c1bb2033
jtithy <tithyjebunnesa202@gmail.com>
2026-07-10 19:38:32 +0600

    Program to format string

---
+++ FormattedStrings.py
@@ -0,0 +1,13 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Program to format string in different ways.
+
+first = 'Hello'
+last = 'Earth'
+#Concatenation
+message = first + ', ' +last+ '!'
+
+#formatted
+msg = f'{first}, {last}!'
+print(message)
+print(msg)
