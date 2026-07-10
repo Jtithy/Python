ad984284cd99d1e7631b7ed321142e46c74d593c
jtithy <tithyjebunnesa202@gmail.com>
2026-07-10 19:56:24 +0600

    String method implementation

---
+++ StringMethods.py
@@ -0,0 +1,12 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Program to demonstrate string methods.
+
+city = 'Bandarban, Bangladesh'
+print(len(city)) #String length
+print(city.upper()) #Convert to uppercase
+print(city.lower()) #Convert to lowercase
+print(city.title()) #Convert to title case
+print(city.find('Bandarban')) #Find the index of a substring
+print(city.replace('Bandarban', 'Chittagong')) #Replace a substring
+print('Bandarban' in city) #Check if substring exists
