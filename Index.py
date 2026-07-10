7b825c7a89d4f14201c747100eef318d1f25c149
jtithy <tithyjebunnesa202@gmail.com>
2026-07-10 19:33:37 +0600

    String indexing

---
+++ Index.py
@@ -0,0 +1,21 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Program to demonstrater index.
+
+
+course = 'Python for Beginners'
+#Whole string
+print(course)
+#1st char of string
+print(course[0])
+#Negative index = last of the string
+print(course[-1])
+#Slicing the string
+print(course[0:6])
+
+#Copying the string
+another = course[:]
+print(another)
+
+name = 'Tithy'
+print(name[1:-1])
