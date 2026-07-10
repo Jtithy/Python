8a358dee19282914fdd771bb30d39f318f414c69
jtithy <tithyjebunnesa202@gmail.com>
2026-07-10 19:19:44 +0600

    Diclaring datatype

---
+++ DataType.py
@@ -0,0 +1,10 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Program to demonstrate data types.
+
+birth_year = input('Enter birth year: ')
+print(type(birth_year))
+age = 2026 - int(birth_year)
+print(type(age))
+
+print(age)
