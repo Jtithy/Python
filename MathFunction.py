d44b2dcd3c82ea9cc69e4ab93d13489fd7bb51eb
jtithy <tithyjebunnesa202@gmail.com>
2026-07-11 08:09:54 +0600

    Implementation of Math Functions in Python.

---
+++ MathFunction.py
@@ -0,0 +1,16 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Implementation of Math Functions in Python.
+#Python Math Module (Google if for more details)
+
+import math
+x = 2.9
+y =2.1
+print(round(x)) #Round to nearest integer
+print(round(y))
+
+a = -2.9
+print(abs(a)) #Absolute(abs) = Provides positive value
+
+print(math.ceil(2.9)) #Rounds up to nearest
+print(math.floor(2.9)) #Rounds down to nearest
