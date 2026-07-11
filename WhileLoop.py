962702ceb70fb6535c9502144fac6042a0e56fe4
jtithy <tithyjebunnesa202@gmail.com>
2026-07-11 23:05:37 +0600

    Implementing while loop.

---
+++ WhileLoop.py
@@ -0,0 +1,10 @@
+#Author: Tithy
+#Date: 2026-07-11
+#Description: Implementing while loop.
+#while condition: then increment/decrement
+
+i = 0
+while i<= 10:
+    print('*' * i)
+    i +=1
+print("Completed")
