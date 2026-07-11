771c1f0031e406fe6cea3d388cbdf061a7da2354
jtithy <tithyjebunnesa202@gmail.com>
2026-07-11 08:02:02 +0600

    Operator Precedence in Python.

---
+++ OperatorPrecedence.py
@@ -0,0 +1,15 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Operator Precedence in Python.
+#Operator precedence determines the order in which operations are performed in an expression.
+
+#Order of precedence: 
+# () (Parantheses)
+# ** (Power)
+# *, /, % (Mul, Div, Mod)
+# +, - (Add, Sub)
+
+x = 10+5*2/3
+y = (10+5)*2//3
+print(x)
+print(y)
