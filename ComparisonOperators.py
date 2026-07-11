2645e49a86f559bf363bb3efcb3071ff272a996a
jtithy <tithyjebunnesa202@gmail.com>
2026-07-11 20:26:41 +0600

    Implementing comparison operators in Python.

---
+++ ComparisonOperators.py
@@ -0,0 +1,16 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Implementing comparison operators in Python.
+
+temperature = float(input("Enter today's temperature in celsius: "))
+
+if temperature < 5:
+    print("It's a very cold day.")
+elif temperature > 5 and temperature < 15:
+    print("It's a cold day.")
+elif temperature > 15 and temperature < 27:
+    print("It's a warm day.")
+elif temperature > 27 and temperature < 40:
+    print("It's a hot day.")
+elif temperature > 40:
+    print("It's a very hot day.")
