2b595070711e357caeb89a1600e45e9dfc69e7f7
jtithy <tithyjebunnesa202@gmail.com>
2026-07-11 19:54:43 +0600

    Average of five numbers taking inputs.

---
+++ Task5.py
@@ -0,0 +1,12 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Average of five numbers taking inputs.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
+num5 = int(input("Enter fifth number: "))
+
+average_number = (num1+num2+num3+num4+num5)/5
+print(f"The average is: {average_number:.2f}")
