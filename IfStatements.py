7f3bcede146f0ee1f5c2b1546af5032a497d0075
jtithy <tithyjebunnesa202@gmail.com>
2026-07-11 08:17:19 +0600

    Implementation of conditional statements.

--- IfStatements.py
+++ IfStatements.py
@@ -1,6 +1,6 @@
 #Author: Tithy
 #Date: 2026-07-10
-#Description: Implementation os conditional statements.
+#Description: Implementation of conditional statements.
 
 is_hot = input("It's a hot day? (y/n): ")
 
