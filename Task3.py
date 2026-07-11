104a05236a83a7c253a0a95c9ff8142ec31cc075
jtithy <tithyjebunnesa202@gmail.com>
2026-07-11 08:30:42 +0600

    Calculate area of a circle

---
+++ Task3.py
@@ -0,0 +1,9 @@
+#Author: Tithy
+#Date: 2026-07-10
+#Description: Calculate area of a circle
+
+import math
+
+radius = float(input("Enter the radius: "))
+area = math.pi * radius ** 2
+print(f"The area is: {area}")
