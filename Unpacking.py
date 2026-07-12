5c22e28148fa5c4899a25e8d5f34f83a4b5d105b
jtithy <tithyjebunnesa202@gmail.com>
2026-07-12 19:22:41 +0600

    Implementing unpacking a tuple in Python.

---
+++ Unpacking.py
@@ -0,0 +1,12 @@
+#Author: Tithy
+#Date: 2026-07-12
+#Description: Implementing unpacking a tuple in Python.
+
+coordinates = (1, 2, 3)
+#x= coordinates[0]
+#y = coordinates[1]
+#z = coordinates[2]
+x, y, z = coordinates
+print("X: ", x)
+print("Y: ", y)
+print("Z: ", z)
