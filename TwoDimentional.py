df37a29a78a61e831a1cd68a6b163226286a0e0d
jtithy <tithyjebunnesa202@gmail.com>
2026-07-12 13:03:28 +0600

    2D list and operations in Python.

---
+++ TwoDimentional.py
@@ -0,0 +1,16 @@
+#Author: Tithy
+#Date: 2026-07-12
+#Description: 2D list and operations in Python.
+
+
+matrix = [
+    [1, 2, 3],
+    [2, 3, 4],
+    [3, 4, 5]
+]
+
+for row in matrix:
+    for item in row:
+        # print(item)
+        print(item, end=' ')
+    print()
