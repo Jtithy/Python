29079afca47897fa9bfa37b2ef76778ca8c372a5
jtithy <tithyjebunnesa202@gmail.com>
2026-07-12 19:15:37 +0600

    Implementing tuples in Python.

---
+++ Tuples.py
@@ -0,0 +1,9 @@
+#Author: Tithy
+#Date: 2026-07-12
+#Description: Implementing tuples in Python.
+#Tuples are similar to lists but can't modify like lists. It's immutable.
+
+numbers = (1, 2, 3, 3, 2, 4, 5, 3)
+print("Tuple: ", numbers)
+print("Count of 3: ", numbers.count(3))
+print("Index of 1: ", numbers.index(1))
