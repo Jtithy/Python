0c996b224b9197e91f98949cf5125c4e885331ce
jtithy <tithyjebunnesa202@gmail.com>
2026-07-12 11:58:19 +0600

    Implement nested loops in Python.

---
+++ NestedLoop.py
@@ -0,0 +1,8 @@
+#Author: Tithy
+#Date: 2026-07-12
+#Description: Implement nested loops in Python.
+#Nested Loop = One loop into another loop.
+
+for x in range(4):
+    for y in range(3):
+        print(f'({x}, {y})')
