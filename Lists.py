06a043bff4c5cba41fd53067dad34b85c42239ff
jtithy <tithyjebunnesa202@gmail.com>
2026-07-12 12:54:09 +0600

    Uderstanding lists and its operation in Python.

---
+++ Lists.py
@@ -0,0 +1,10 @@
+#Author: Tithy
+#Date: 2026-07-12
+#Description: Uderstanding lists and its operation in Python.
+
+names = ['John', 'Alice', 'Bob', 'Eve', 'David', 'Jane']
+
+print("Original: ", names)
+print("First Element: ", names[0])
+print("Last Element: ", names[-1])
+print("Sliced: ", names[2: 4])
