182d5e7aeae48072e98dcd6645e9d430cfb8619a
jtithy <tithyjebunnesa202@gmail.com>
2026-07-13 21:00:26 +0600

    Return Statements implementation.

---
+++ ReturnStatements.py
@@ -0,0 +1,14 @@
+# Author: Tithy
+# Date: 2026-07-13
+# Description: Return Statements implementation.
+
+def square(num):
+    return num * num
+
+print(square(5))
+
+
+def add(a, b):
+    print(a+b)
+    
+print(add(5, 10)) #By default print none because add function doesn't have return statemnet
