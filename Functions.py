81c1d5bca414c84003eb941a8edff8adf5b8b481
jtithy <tithyjebunnesa202@gmail.com>
2026-07-13 20:32:07 +0600

    Implementing functions in Python.

---
+++ Functions.py
@@ -0,0 +1,11 @@
+# Author: Tithy
+# Date: 2026-07-13
+# Description: Implementing functions in Python.
+# Function - Reusable blocks of code that performs a specific task.
+# Always define function before function calling.
+def greet_user():
+    print("Hi there! Welcome to the Python program.")
+    
+print("Start")
+greet_user()
+print("Finish")
