99f6a8ea906a2d274b0fc3f961314a69ba723b07
jtithy <tithyjebunnesa202@gmail.com>
2026-07-13 19:59:30 +0600

    Implement emoji converter using dictionary mapping.

--- EmojiConverter.py
+++ EmojiConverter.py
@@ -1,4 +1,14 @@
 # Author: Tithy
-# Date: 2026-07-12
+# Date: 2026-07-13
 # Description: Implement emoji converter using dictionary mapping.
 
+message = input(">")
+words = message.split(' ')
+emojis = {
+    ":)" : "😊",
+    ":(" : "😞"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
