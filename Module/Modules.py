e21e77ea8eed42ae2b01a910d8ce9a51fb3a6ebe
jtithy <tithyjebunnesa202@gmail.com>
2026-07-15 15:52:02 +0600

    Implementing modules in Python.

---
+++ Module/Modules.py
@@ -0,0 +1,10 @@
+# Author: Tithy
+# Date: 2026-07-14
+# Description: 
+# Module - A file containg code that can be imported and used in other Python code.
+
+# Importing the converter module
+import Converter
+
+print("Weight in pounds: ", Converter.kg_to_lbs(70))
+print("Weight in kilograms: ", Converter.lbs_to_kg(44))
