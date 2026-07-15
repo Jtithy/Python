0bf34513810e424d326df19c48be649fbf4e4a8c
jtithy <tithyjebunnesa202@gmail.com>
2026-07-15 15:58:45 +0600

    Importing specific function from the Converter modlue

--- Module/Modules.py
+++ Module/Modules.py
@@ -3,8 +3,13 @@
 # Description: 
 # Module - A file containg code that can be imported and used in other Python code.
 
-# Importing the converter module
+# Importing the whole converter module
 import Converter
 
 print("Weight in pounds: ", Converter.kg_to_lbs(70))
 print("Weight in kilograms: ", Converter.lbs_to_kg(44))
+
+# Importing specific function from the Converter modlue
+from Converter import kg_to_lbs
+
+print("Weight in pounds: ", kg_to_lbs(45))
