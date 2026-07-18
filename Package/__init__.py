b40faf635873455fef0b01fc596e13bd92e923c0
jtithy <tithyjebunnesa202@gmail.com>
2026-07-18 19:50:45 +0600

    Implementing packages in Python

---
+++ Package/__init__.py
@@ -0,0 +1,15 @@
+# Author: Tithy
+# Date: 2026-07-18
+# Description: Implementing packages in Python
+#Package - Collection of modules.
+
+# import Package.shipping
+#Package.shipping.cal_shipping()
+
+#from Package import shipping
+#shipping.cal_shipping()
+
+
+from Package.shipping import cal_shipping
+
+cal_shipping()
---
+++ Package/shipping.py
@@ -0,0 +1,6 @@
+# Author: Tithy
+# Date: 2026-07-18
+# Description: Implementing packages in Python.
+
+def cal_shipping():
+    print("Calculating shipping cost...")
