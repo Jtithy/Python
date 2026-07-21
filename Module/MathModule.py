a8af0dac3f5ed6b23d9dec6136393ea948154b02
jtithy <tithyjebunnesa202@gmail.com>
2026-07-21 07:13:03 +0600

    Math module demonstration.

---
+++ Module/MathModule.py
@@ -0,0 +1,9 @@
+#Author: Tithy
+#Date: 2026-07-21
+#Description: Math module demonstration.
+
+import math
+
+degree = float(input("Enter degree: "))
+rads = math.radians(degree)
+print("Radiants: ", math.sin(rads))
