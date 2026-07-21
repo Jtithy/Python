e65213447be37dbd1bedf7ecaee409faad050c7b
jtithy <tithyjebunnesa202@gmail.com>
2026-07-21 07:28:32 +0600

    OS module demonstration.

---
+++ Module/OSModule.py
@@ -0,0 +1,11 @@
+#Author: Tithy
+#Date: 2026-07-21
+#Description: OS module demonstration.
+
+import os
+#Get current working directory
+print(os.getcwd())
+#Get list of files and directories in current workng directory
+print(os.listdir())
+#Locate the path of the OS module
+print(os.__file__)
