b8f90c9304fde8ec8303ddb733ca78c43f7f3156
jtithy <tithyjebunnesa202@gmail.com>
2026-07-23 08:37:38 +0600

    Create your own package containing at least three modules in Python.

---
+++ All_Tasks/Task44.py/main.py
@@ -0,0 +1,14 @@
+#Author: Tithy
+#Date: 2026-07-21
+#Description: Create your own package containing at least three modules in Python.
+
+# main.py
+
+from math_utils import add, subtract, multiply
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
