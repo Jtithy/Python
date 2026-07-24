5eec7ec84c84f660b09ceb0dc476373c72dd2f35
jtithy <tithyjebunnesa202@gmail.com>
2026-07-24 09:33:02 +0600

    File write

---
+++ FileHandling/FileWrite.py
@@ -0,0 +1,8 @@
+#Author: Tithy
+#Date: 2026-07-24
+#Description: Program to demonstrate file handling in Python.
+
+
+with open ('d:\\03_AllAboutPython\\Python\\FileHandling\\text2.txt', 'w') as f:
+    f.write("This test2 file.")
+    f.write("This is second line.")
